f89c1e4e3da21de6eb88149e68043870f9d7d8e4
LasyaE04 <lasyaE04@gmail.com>
2025-11-30 02:20:38 -0500

    Modified outputfile structure

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ cd bin
 
 for i in {1..20}; do
     ./interrupts_RR ../input_files/test${i}.txt
-    cp execution.txt ../output_files/execution${i}_RR.txt
-    cp memory_status.txt ../output_files/memory_statuses/memory_status${i}_RR.txt
+    cp execution.txt ../output_files/output_RR/execution${i}_RR.txt
+    cp memory_status.txt ../output_files/output_RR/memory_statuses/memory_status${i}_RR.txt
 
     ./interrupts_EP_RR ../input_files/test${i}.txt
-    cp execution.txt ../output_files/execution${i}_EP_RR.txt
-    cp memory_status.txt ../output_files/memory_statuses/memory_status${i}_EP_RR.txt
+    cp execution.txt ../output_files/output_EP_RR/execution${i}_EP_RR.txt
+    cp memory_status.txt ../output_files/output_EP_RR/memory_statuses/memory_status${i}_EP_RR.txt
 done
